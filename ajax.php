d5ae738b81a542d3fc66bdadb09ac53ae17c9909
Nic Haynes <Nic@Parker.local>
2010-12-02 18:04:38 -0600

    Added get request to ajax for remote calls

--- ajax.php
+++ ajax.php
@@ -17,4 +17,18 @@ if ($_GET['action'] == "getDefaultSettings") {
 //  print_r($_GET);
   echo defaultSettings($_GET['id'], $_GET['method']);
 }
+
+if ($_GET['action'] == "makeHash") {
+	$master = $_GET['master'];
+	$host = strtolower($_GET['host']);
+	$s = $_GET['settings'];
+
+	// Select algorithm
+	$alg = $s['h_algorithm']."Hash";
+	
+	// Create the new password
+	$out = $alg($master, $host, $s);	
+	echo $out;
+}
+
 ?>
